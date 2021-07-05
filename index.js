de1dbdfeb1b17d1211a58fe609962414b36642f1
shravya <shravya.kasturi@leanix.net>
2021-07-05 11:52:37 +0530

    feat():change made to files

--- index.js
+++
@@ -1,8 +0,0 @@
-function run() {
-	console.log("this is new npm package working")
-	return "Working"
-}
-
-export {
-	run
-}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+function run() {
+  console.log('this is new npm package working');
+  return 'Working';
+}
+
+export { run };
