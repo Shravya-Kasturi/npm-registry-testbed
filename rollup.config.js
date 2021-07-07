4b2b38c43575989fc2b814d4528bac27c7f32d4d
shravya <shravya.kasturi@leanix.net>
2021-07-07 13:54:39 +0530

    build():add rollup

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.es.js',
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+  plugins: [typescript({ tsconfig: 'tsconfig.build.json' }), terser(), commonjs(), nodeResolve({ browser: true })]
+};
